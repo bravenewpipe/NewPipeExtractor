InfinityLoop1308/PipePipeExtractor

searchfilters: convert bandcamp to new framework
Available content filters:
- All
- artists & labels
- albums
- tracks

extractor/src/main/java/org/schabi/newpipe/extractor/services/bandcamp/linkHandler/BandcampSearchQueryHandlerFactory.java
@@ -2,6 +2,10 @@
 
 package org.schabi.newpipe.extractor.services.bandcamp.linkHandler;
 
+import org.schabi.newpipe.extractor.services.bandcamp.search.filter.BandcampFilters;
+import org.schabi.newpipe.extractor.search.filter.Filter;
+import org.schabi.newpipe.extractor.search.filter.FilterItem;
+
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandlerFactory;
 
@@ -13,15 +17,34 @@ import static org.schabi.newpipe.extractor.services.bandcamp.extractors.Bandcamp
 
 public class BandcampSearchQueryHandlerFactory extends SearchQueryHandlerFactory {
 
+    private final BandcampFilters searchFilters = new BandcampFilters();
 
     @Override
     public String getUrl(final String query,
-                         final List<String> contentFilter,
-                         final String sortFilter) throws ParsingException {
+                         final List<FilterItem> selectedContentFilter,
+                         final List<FilterItem> selectedSortFilter) throws ParsingException {
+
+
+        searchFilters.setSelectedSortFilter(selectedSortFilter);
+        searchFilters.setSelectedContentFilter(selectedContentFilter);
+
+        final String filterQuery = searchFilters.evaluateSelectedContentFilters();
+
         try {
-            return BASE_URL + "/search?q=" + URLEncoder.encode(query, "UTF-8") + "&page=1";
+            return BASE_URL + "/search?q=" + URLEncoder.encode(query, "UTF-8")
+                    + filterQuery + "&page=1";
         } catch (final UnsupportedEncodingException e) {
             throw new ParsingException("query \"" + query + "\" could not be encoded", e);
         }
+    }
+
+    @Override
+    public Filter getAvailableContentFilter() {
+        return searchFilters.getContentFilters();
+    }
+
+    @Override
+    public FilterItem getFilterItem(final int filterId) {
+        return searchFilters.getFilterItem(filterId);
     }
 }

bravenewpipe/NewPipeExtractor

extractor/src/main/java/org/schabi/newpipe/extractor/services/bandcamp/linkHandler/BandcampSearchQueryHandlerFactory.java
@@ -2,6 +2,10 @@
 
 package org.schabi.newpipe.extractor.services.bandcamp.linkHandler;
 
+import org.schabi.newpipe.extractor.services.bandcamp.search.filter.BandcampFilters;
+import org.schabi.newpipe.extractor.search.filter.FilterContainer;
+import org.schabi.newpipe.extractor.search.filter.FilterItem;
+
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandlerFactory;
 
@@ -13,15 +17,34 @@ import static org.schabi.newpipe.extractor.services.bandcamp.extractors.Bandcamp
 
 public class BandcampSearchQueryHandlerFactory extends SearchQueryHandlerFactory {
 
+    private final BandcampFilters searchFilters = new BandcampFilters();
 
     @Override
     public String getUrl(final String query,
-                         final List<String> contentFilter,
-                         final String sortFilter) throws ParsingException {
+                         final List<FilterItem> selectedContentFilter,
+                         final List<FilterItem> selectedSortFilter) throws ParsingException {
+
+
+        searchFilters.setSelectedSortFilter(selectedSortFilter);
+        searchFilters.setSelectedContentFilter(selectedContentFilter);
+
+        final String filterQuery = searchFilters.evaluateSelectedContentFilters();
+
         try {
-            return BASE_URL + "/search?q=" + URLEncoder.encode(query, "UTF-8") + "&page=1";
+            return BASE_URL + "/search?q=" + URLEncoder.encode(query, "UTF-8")
+                    + filterQuery + "&page=1";
         } catch (final UnsupportedEncodingException e) {
             throw new ParsingException("query \"" + query + "\" could not be encoded", e);
         }
+    }
+
+    @Override
+    public FilterContainer getAvailableContentFilter() {
+        return searchFilters.getContentFilters();
+    }
+
+    @Override
+    public FilterItem getFilterItem(final int filterId) {
+        return searchFilters.getFilterItem(filterId);
     }
 }