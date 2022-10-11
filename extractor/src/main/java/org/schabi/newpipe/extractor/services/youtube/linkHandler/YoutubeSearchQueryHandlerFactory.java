InfinityLoop1308/PipePipeExtractor

searchfilters: convert youtube to new framework
Available content filters:
Youtube
- all
- videos
- channels
- playlists

Youtube Music
- Songs
- Videos
- Albums
- Playlists
- Artists

Available sort filters:
- 'Sort by'
- Upload Date
- Duration
- Features

extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/linkHandler/YoutubeSearchQueryHandlerFactory.java
@@ -1,15 +1,15 @@
 package org.schabi.newpipe.extractor.services.youtube.linkHandler;
 
+import org.schabi.newpipe.extractor.search.filter.Filter;
+import org.schabi.newpipe.extractor.search.filter.FilterItem;
+import org.schabi.newpipe.extractor.services.youtube.search.filter.YoutubeFilters;
+
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandlerFactory;
 
 import javax.annotation.Nonnull;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.List;
 
-import static org.schabi.newpipe.extractor.utils.Utils.UTF_8;
-import static org.schabi.newpipe.extractor.utils.Utils.isNullOrEmpty;
+import java.util.List;
 
 public final class YoutubeSearchQueryHandlerFactory extends SearchQueryHandlerFactory {
 
@@ -17,7 +17,6 @@ public final class YoutubeSearchQueryHandlerFactory extends SearchQueryHandlerFa
     public static final String VIDEOS = "videos";
     public static final String CHANNELS = "channels";
     public static final String PLAYLISTS = "playlists";
-    public static final String LIVES = "lives";
 
     public static final String MUSIC_SONGS = "music_songs";
     public static final String MUSIC_VIDEOS = "music_videos";
@@ -25,8 +24,7 @@ public final class YoutubeSearchQueryHandlerFactory extends SearchQueryHandlerFa
     public static final String MUSIC_PLAYLISTS = "music_playlists";
     public static final String MUSIC_ARTISTS = "music_artists";
 
-    private static final String SEARCH_URL = "https://www.youtube.com/results?search_query=";
-    private static final String MUSIC_SEARCH_URL = "https://music.youtube.com/search?q=";
+    private final YoutubeFilters searchFilters = new YoutubeFilters();
 
     @Nonnull
     public static YoutubeSearchQueryHandlerFactory getInstance() {
@@ -35,81 +33,30 @@ public final class YoutubeSearchQueryHandlerFactory extends SearchQueryHandlerFa
 
     @Override
     public String getUrl(final String searchString,
-                         @Nonnull final List<String> contentFilters,
-                         final String sortFilter) throws ParsingException {
-        try {
-            if (!contentFilters.isEmpty()) {
-                final String contentFilter = contentFilters.get(0);
-                switch (contentFilter) {
-                    case ALL:
-                    default:
-                        break;
-                    case VIDEOS:
-                        return SEARCH_URL + URLEncoder.encode(searchString, UTF_8)
-                                + "&sp=EgIQAQ%253D%253D";
-                    case CHANNELS:
-                        return SEARCH_URL + URLEncoder.encode(searchString, UTF_8)
-                                + "&sp=EgIQAg%253D%253D";
-                    case PLAYLISTS:
-                        return SEARCH_URL + URLEncoder.encode(searchString, UTF_8)
-                                + "&sp=EgIQAw%253D%253D";
-                    case LIVES:
-                        return SEARCH_URL + URLEncoder.encode(searchString, UTF_8)
-                                + "&sp=EgJAAQ%253D%253D";
-                    case MUSIC_SONGS:
-                    case MUSIC_VIDEOS:
-                    case MUSIC_ALBUMS:
-                    case MUSIC_PLAYLISTS:
-                    case MUSIC_ARTISTS:
-                        return MUSIC_SEARCH_URL + URLEncoder.encode(searchString, UTF_8);
-                }
-            }
-
-            return SEARCH_URL + URLEncoder.encode(searchString, UTF_8);
-        } catch (final UnsupportedEncodingException e) {
-            throw new ParsingException("Could not encode query", e);
-        }
+                         @Nonnull final List<FilterItem> selectedContentFilter,
+                         final List<FilterItem> selectedSortFilter) throws ParsingException {
+        searchFilters.setSelectedContentFilter(selectedContentFilter);
+        searchFilters.setSelectedSortFilter(selectedSortFilter);
+        return searchFilters.evaluateSelectedFilters(searchString);
     }
 
     @Override
-    public String[] getAvailableContentFilter() {
-        return new String[]{
-                ALL,
-                VIDEOS,
-                CHANNELS,
-                LIVES,
-                PLAYLISTS,
-                MUSIC_SONGS,
-                MUSIC_VIDEOS,
-                MUSIC_ALBUMS,
-                MUSIC_PLAYLISTS
-                // MUSIC_ARTISTS
-        };
+    public Filter getAvailableContentFilter() {
+        return searchFilters.getContentFilters();
     }
 
-    @Nonnull
-    public static String getSearchParameter(final String contentFilter) {
-        if (isNullOrEmpty(contentFilter)) {
-            return "";
-        }
+    @Override
+    public Filter getAvailableSortFilter() {
+        return searchFilters.getSortFilters();
+    }
 
-        switch (contentFilter) {
-                case VIDEOS:
-                    return "EgIQAQ%3D%3D";
-                case CHANNELS:
-                    return "EgIQAg%3D%3D";
-                case PLAYLISTS:
-                    return "EgIQAw%3D%3D";
-                case LIVES:
-                    return "EgJAAQ%3D%3D";
-                case ALL:
-                case MUSIC_SONGS:
-                case MUSIC_VIDEOS:
-                case MUSIC_ALBUMS:
-                case MUSIC_PLAYLISTS:
-                case MUSIC_ARTISTS:
-                default:
-                    return "";
-        }
+    @Override
+    public Filter getContentFilterSortFilterVariant(final int contentFilterId) {
+        return searchFilters.getContentFilterSortFilterVariant(contentFilterId);
+    }
+
+    @Override
+    public FilterItem getFilterItem(final int filterId) {
+        return searchFilters.getFilterItem(filterId);
     }
 }

bravenewpipe/NewPipeExtractor

extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/linkHandler/YoutubeSearchQueryHandlerFactory.java
@@ -1,15 +1,15 @@
 package org.schabi.newpipe.extractor.services.youtube.linkHandler;
 
+import org.schabi.newpipe.extractor.search.filter.FilterContainer;
+import org.schabi.newpipe.extractor.search.filter.FilterItem;
+import org.schabi.newpipe.extractor.services.youtube.search.filter.YoutubeFilters;
+
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandlerFactory;
 
 import javax.annotation.Nonnull;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.List;
 
-import static org.schabi.newpipe.extractor.utils.Utils.UTF_8;
-import static org.schabi.newpipe.extractor.utils.Utils.isNullOrEmpty;
+import java.util.List;
 
 public final class YoutubeSearchQueryHandlerFactory extends SearchQueryHandlerFactory {
 
@@ -24,8 +24,7 @@ public final class YoutubeSearchQueryHandlerFactory extends SearchQueryHandlerFa
     public static final String MUSIC_PLAYLISTS = "music_playlists";
     public static final String MUSIC_ARTISTS = "music_artists";
 
-    private static final String SEARCH_URL = "https://www.youtube.com/results?search_query=";
-    private static final String MUSIC_SEARCH_URL = "https://music.youtube.com/search?q=";
+    private final YoutubeFilters searchFilters = new YoutubeFilters();
 
     @Nonnull
     public static YoutubeSearchQueryHandlerFactory getInstance() {
@@ -34,75 +33,25 @@ public final class YoutubeSearchQueryHandlerFactory extends SearchQueryHandlerFa
 
     @Override
     public String getUrl(final String searchString,
-                         @Nonnull final List<String> contentFilters,
-                         final String sortFilter) throws ParsingException {
-        try {
-            if (!contentFilters.isEmpty()) {
-                final String contentFilter = contentFilters.get(0);
-                switch (contentFilter) {
-                    case ALL:
-                    default:
-                        break;
-                    case VIDEOS:
-                        return SEARCH_URL + URLEncoder.encode(searchString, UTF_8)
-                                + "&sp=EgIQAQ%253D%253D";
-                    case CHANNELS:
-                        return SEARCH_URL + URLEncoder.encode(searchString, UTF_8)
-                                + "&sp=EgIQAg%253D%253D";
-                    case PLAYLISTS:
-                        return SEARCH_URL + URLEncoder.encode(searchString, UTF_8)
-                                + "&sp=EgIQAw%253D%253D";
-                    case MUSIC_SONGS:
-                    case MUSIC_VIDEOS:
-                    case MUSIC_ALBUMS:
-                    case MUSIC_PLAYLISTS:
-                    case MUSIC_ARTISTS:
-                        return MUSIC_SEARCH_URL + URLEncoder.encode(searchString, UTF_8);
-                }
-            }
-
-            return SEARCH_URL + URLEncoder.encode(searchString, UTF_8);
-        } catch (final UnsupportedEncodingException e) {
-            throw new ParsingException("Could not encode query", e);
-        }
+                         @Nonnull final List<FilterItem> selectedContentFilter,
+                         final List<FilterItem> selectedSortFilter) throws ParsingException {
+        searchFilters.setSelectedContentFilter(selectedContentFilter);
+        searchFilters.setSelectedSortFilter(selectedSortFilter);
+        return searchFilters.evaluateSelectedFilters(searchString);
     }
 
     @Override
-    public String[] getAvailableContentFilter() {
-        return new String[]{
-                ALL,
-                VIDEOS,
-                CHANNELS,
-                PLAYLISTS,
-                MUSIC_SONGS,
-                MUSIC_VIDEOS,
-                MUSIC_ALBUMS,
-                MUSIC_PLAYLISTS
-                // MUSIC_ARTISTS
-        };
+    public FilterContainer getAvailableContentFilter() {
+        return searchFilters.getContentFilters();
     }
 
-    @Nonnull
-    public static String getSearchParameter(final String contentFilter) {
-        if (isNullOrEmpty(contentFilter)) {
-            return "";
-        }
+    @Override
+    public FilterContainer getContentFilterSortFilterVariant(final int contentFilterId) {
+        return searchFilters.getContentFilterSortFilterVariant(contentFilterId);
+    }
 
-        switch (contentFilter) {
-                case VIDEOS:
-                    return "EgIQAQ%3D%3D";
-                case CHANNELS:
-                    return "EgIQAg%3D%3D";
-                case PLAYLISTS:
-                    return "EgIQAw%3D%3D";
-                case ALL:
-                case MUSIC_SONGS:
-                case MUSIC_VIDEOS:
-                case MUSIC_ALBUMS:
-                case MUSIC_PLAYLISTS:
-                case MUSIC_ARTISTS:
-                default:
-                    return "";
-        }
+    @Override
+    public FilterItem getFilterItem(final int filterId) {
+        return searchFilters.getFilterItem(filterId);
     }
 }