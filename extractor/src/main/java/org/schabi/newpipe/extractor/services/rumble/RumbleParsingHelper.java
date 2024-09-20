c1f839e4f8e8c2472ce3a72dd14e30e82b8e7777
evermind <evermind@tuxfamily.org>
2024-09-20 14:37:03 +0200

    RUMBLE: fix extraction of video id for older java versions

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/rumble/RumbleParsingHelper.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/rumble/RumbleParsingHelper.java
@@ -220,13 +220,13 @@ public final class RumbleParsingHelper {
     }
 
     public static String getEmbedVideoId(final String rb) {
-        final String VALID_URL = "https?://(?:www\\.)?rumble\\.com/embed/(?:[0-9a-z]+\\.)?(?<id>[0-9a-z]+)";
-        final String EMBED_REGEX = "(?:<(?:script|iframe)[^>]+\\bsrc=|[\"']embedUrl[\"']\\s*:\\s*)[\"'](?<url>" + VALID_URL + ")";
+        final String VALID_URL = "https?://(?:www\\.)?rumble\\.com/embed/(?:[0-9a-z]+\\.)?([0-9a-z]+)"; // id is group 1
+        final String EMBED_REGEX = "(?:<(?:script|iframe)[^>]+\\bsrc=|[\"']embedUrl[\"']\\s*:\\s*)[\"']" + VALID_URL;
         Pattern pattern = Pattern.compile(EMBED_REGEX);
         Matcher matcher = pattern.matcher(rb);
         if (matcher.find()) {
             // Remove v (first character) from the id
-            return matcher.group(2).substring(1);
+            return matcher.group(1).substring(1);
         } else {
             return null;
         }
